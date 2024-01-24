e960cb333dbdcf24137952794332ebefcc98a022
thor <thor@colourbox.com>
2024-01-24 22:52:21 +0100

    Initial work on chip8_core.

---
+++ chip8_core/src/lib.rs
@@ -0,0 +1,128 @@
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const FONTSET_SIZE: usize = 5 * 16; // 5 bytes per character, 16 characters total
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+const START_ADDRESS: u16 = 0x200;
+
+const RAM_SIZE: usize = 4096;
+const NUM_REGISTERS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+
+pub struct Emulator {
+    program_counter: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGISTERS],
+    i_reg: u16,
+    stack_pointer: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    delay_timer: u8,
+    sound_timer: u8,
+}
+
+impl Emulator {
+    pub fn new() -> Self {
+        let mut new_emulator = Self {
+            program_counter: START_ADDRESS,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGISTERS],
+            i_reg: 0,
+            stack_pointer: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            delay_timer: 0,
+            sound_timer: 0,
+        };
+        new_emulator.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emulator
+    }
+
+    pub fn reset(&mut self) {
+        self.program_counter = START_ADDRESS;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGISTERS];
+        self.i_reg = 0;
+        self.stack_pointer = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+    }
+
+    fn push(&mut self, value: u16) {
+        self.stack[self.stack_pointer as usize] = value;
+        self.stack_pointer += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        if self.stack_pointer < 1 {
+            panic!("Stack underflow, popping empty stack");
+        }
+        self.stack_pointer -= 1;
+        self.stack[self.stack_pointer as usize]
+    }
+
+    pub fn tick(&mut self) {
+        // Fetch
+        let opcode = self.fetchOpcode();
+        // Decode
+        self.execute(opcode);
+        // Execute
+    }
+
+    fn execute(&mut self, opcode: u16) {
+        let digit1 = (opcode & 0xF000) >> 12;
+        let digit2 = (opcode & 0x0F00) >> 8;
+        let digit3 = (opcode & 0x00F0) >> 4;
+        let digit4 = opcode & 0x000F;
+        match (digit1, digit2, digit3, digit4) {
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", opcode),
+        }
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+
+        if self.sound_timer > 0 {
+            if self.sound_timer == 1 {
+                // BEEP
+            }
+            self.sound_timer -= 1;
+        }
+    }
+
+    fn fetchOpcode(&mut self) -> u16 {
+        let higher_byye = self.ram[self.program_counter as usize] as u16;
+        let lower_byte = self.ram[(self.program_counter + 1) as usize] as u16;
+        let opcode = (higher_byye << 8) | lower_byte;
+        self.program_counter += 2;
+        opcode
+    }
+}
